facdf78336c9f9af997b745edd63fb8c8e54aff6
15765-akaha <turning2012@gmail.com>
2019-12-26 16:43:21 +0900

    [finish]5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'bc27e4db9770616d2b1e325745b65363a6608a236d51410bdfec97dd233c7b4f9381f6c69ddb4499af3362791ef4e2ab476407a6e820317a0d1a0b2119c6319c'
