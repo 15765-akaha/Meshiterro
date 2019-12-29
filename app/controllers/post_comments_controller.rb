d4ce453d509a47cd20cab4b6800df7158a07d912
15765-akaha <turning2012@gmail.com>
2019-12-29 22:32:45 +0900

    [finish]5-6

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -9,7 +9,7 @@ class PostCommentsController < ApplicationController
 	end
 private
 	def post_comment_params
-    	params.require(:post_comment).permit(:user_id,:post_image_id,:comment)
+    	params.require(:post_comment).permit(:user_id, :post_image_id, :comment)
 	end
 end
 
